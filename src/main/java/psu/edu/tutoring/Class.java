348de398ece7f16b49227d4b3745e1950e0a5889
Greg <gac5206@psu.edu>
2020-02-24 11:41:59 -0500

    Create Class.java

---
+++ src/main/java/psu/edu/tutoring/Class.java
@@ -0,0 +1,25 @@
+package psu.edu.tutoring;
+
+
+public class Class {
+
+    String className;
+    int classNum;
+
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public int getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(int classNum) {
+        this.classNum = classNum;
+    }
+}
