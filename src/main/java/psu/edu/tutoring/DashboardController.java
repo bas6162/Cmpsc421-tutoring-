6b6c572edc8fa6c0c9cb14c9728dacdca0807818
Greg <gac5206@psu.edu>
2020-02-26 10:52:24 -0500

    Create DashboardController.java

---
+++ src/main/java/psu/edu/tutoring/DashboardController.java
@@ -0,0 +1,26 @@
+package psu.edu.tutoring;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+public class DashboardController {
+
+    @GetMapping("/user/classes")
+    public HashMap<String, Class> getUserClasses(@RequestBody User user)
+    {
+        return user.classes;
+
+    }
+
+    @GetMapping("/user/appointments")
+    public HashMap<Integer, Appointment> getUserAppointments(@RequestBody User user)
+    {
+        return user.appointments;
+    }
+
+    
+
+}
