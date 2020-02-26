a6f407df1a3204f61565abd4d2b31dc8c3b104dc
Greg <gac5206@psu.edu>
2020-02-26 10:48:24 -0500

    Create TutorsController.java

---
+++ src/main/java/psu/edu/tutoring/TutorsController.java
@@ -0,0 +1,32 @@
+package psu.edu.tutoring;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+public class TutorsController {
+
+    public HashMap<String, Tutor> tutors = new HashMap<>();
+
+    @PostMapping("/tutor/create")
+    public String createTutor(@RequestBody Tutor tutor)
+    {
+        tutors.put(tutor.getTutorName(), tutor);
+
+        return tutor.getTutorName() + " has been added as a tutor";
+    }
+
+    @DeleteMapping("/tutor/delete")
+    public String deleteTutor(@RequestParam(name="tutorName")String tutorName, @RequestBody Tutor tutor)
+    {
+        tutors.remove(tutorName, tutor);
+
+        return tutorName + " had been removed as a tutor";
+    }
+
+
+
+
+
+}
